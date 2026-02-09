489bd9802ccf2aead3172b7dbc1d6a8193d24791
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-09 10:09:49 +0530

    chnages added

--- backend/server.js
+++ backend/server.js
@@ -7,18 +7,29 @@ const authRoutes = require("./routes/authentication-route");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.send("GYAN backend is running");
+  res.send("GYAN backend is running ");
 });
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(" MongoDB connection error:", err));
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://127.0.0.1:${PORT}`);
+});
