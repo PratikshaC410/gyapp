db38412a07fcf106a08888d85fea3bb7c0a5f761
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-09 12:51:35 +0530

    final fix

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
